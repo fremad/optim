8ec611263f95954a02a04153516c891028cdbaae
larsH <larshjerrild@hotmail.com>
2018-10-25 19:59:28 +0200

    Added datasets and loaded them into the program. Additionally code is provided to display images from both the ORL and the MNIST dataset

---
+++ algo.py
@@ -0,0 +1,14 @@
+def nc_classify(Xtrain,Xtest,train_lbls):
+    print("not yet implemented")
+
+def nsc_classify(Xtrain,K,Xtest, train_lbls):
+    print("not yet implemented")
+
+def train_perceptron_backprop(Xtrain, train_lbls, eta):
+    print("not implemented")
+
+def perceptron_bp_classify(W,Xtest):
+    print("not implemented")
+
+#def k_means_classify(Xtrain, Xtest):
+#    print("not implemented")
--- main.py
+++ main.py
@@ -1,3 +1,34 @@
-print("this is my project")
+import numpy as np
+import matplotlib.pyplot as plt
+import algo as alg
+from mnist import MNIST
 
-print('it works')
+
+def nc_classify(Xtrain,Xtest,train_lbls):
+    print("not yet implemented")
+
+
+#ORL training data and label import
+X = np.transpose(np.genfromtxt('./data/orl_data.txt', delimiter=""))
+lbls = np.genfromtxt('./data/orl_lbls.txt', delimiter="\t")
+
+#MNIST data import
+mndata = MNIST('./data')
+training_images, training_labels = mndata.load_training()
+test_images, test_labels = mndata.load_testing()
+
+
+x = np.array([[3,2],[4,5]])
+y = np.array([2,1])
+
+
+h = [training_images, training_labels]
+#Show MNIST image i
+plt.imshow(np.reshape(training_images[1],(28,28)),cmap='gray')
+plt.show()
+
+#Show ORL image i
+# i = 0
+# img = X[i].reshape((30,40))
+# plt.imshow(img, cmap='gray')
+# plt.show()
