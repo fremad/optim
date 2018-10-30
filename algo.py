08f211fb5dcf60e40a4e3c2a2dccae5186ad4dd0
larsH <larshjerrild@hotmail.com>
2018-10-30 16:07:12 +0100

    ORL now classfied with NCC

--- algo.py
+++ algo.py
@@ -1,5 +1,33 @@
-def nc_classify(Xtrain,Xtest,train_lbls):
-    print("not yet implemented")
+import numpy as np
+import matplotlib.pyplot as plt
+
+def nc_classify(Xtrain,Xtest,train_lbls,N_k):
+
+    #Initialize mu an N_k vectors
+    mu_k = np.zeros((N_k, Xtrain.shape[1]))
+    n_k = np.zeros(N_k)
+
+    #Sum training data into mu_k and count N_k
+    for i in range(Xtrain.shape[0]):
+        mu_k[int(train_lbls[i])] += Xtrain[i]
+        n_k[int(train_lbls[i])] += 1
+
+    #divide with N_k to achive mu_ks
+    for k in range(n_k.shape[0]):
+        if(n_k[k] != 0):
+            mu_k[k] = np.true_divide(mu_k[k], n_k[k])
+
+    #Uncomment to view the mu_ks as pictures
+    # for k in range(n_k.shape[0]):
+    #     plt.imshow(np.reshape(mu_k[k],(28,28)),cmap='gray')
+    #     plt.show()
+
+    lbls = np.zeros(Xtest.shape[0])
+    for k in range(Xtest.shape[0]):
+        tmp = Xtest[k] - mu_k
+        lbls[k] = np.argmin(np.apply_along_axis(np.linalg.norm, 1, tmp))
+
+    return lbls
 
 def nsc_classify(Xtrain,K,Xtest, train_lbls):
     print("not yet implemented")
@@ -12,3 +40,40 @@ def perceptron_bp_classify(W,Xtest):
 
 #def k_means_classify(Xtrain, Xtest):
 #    print("not implemented")
+
+def error_in_percent(true_lbls,model_lbls):
+    errors = 0.0
+    for i in range(true_lbls.shape[0]):
+        if(true_lbls[i] != model_lbls[i]):
+            errors += 1.0
+    return errors/true_lbls.shape[0], errors
+
+
+
+
+
+
+
+
+
+
+#USELESS JUNK!!!!!
+
+# count = 0
+#
+# mu_k = np.zeros((10,784))
+# N_k = np.zeros(10)
+#
+# for i in range(s_training.shape[0]):
+#     mu_k[training_labels[i]] += training_images[i]
+#     N_k[training_labels[i]] += 1
+#
+# for k in range(N_k.shape[0]):
+#     mu_k[k] = np.true_divide(mu_k[k], N_k[k])
+#
+# tmp = test_images[s_test[0]] - mu_k
+# k = np.apply_along_axis(np.linalg.norm, 1,tmp)
+# lbl = np.argmin(k)
+#
+# print('Guess : ',lbl)
+# print('Actual : ', test_labels[s_test[0]])
--- main.py
+++ main.py
@@ -5,33 +5,6 @@ from mnist import MNIST
 
 
 
-def nc_classify(Xtrain,Xtest,train_lbls,N_k):
-
-    #Initialize mu an N_k vectors
-    mu_k = np.zeros((N_k, Xtrain.shape[1]))
-    n_k = np.zeros(N_k)
-
-    #Sum training data into mu_k and count N_k
-    for i in range(Xtrain.shape[0]):
-        mu_k[train_lbls[i]] += Xtrain[i]
-        n_k[train_lbls[i]] += 1
-
-    #divide with N_k to achive mu_ks
-    for k in range(n_k.shape[0]):
-        mu_k[k] = np.true_divide(mu_k[k], N_k[k])
-
-    lbls = np.zeros(Xtest.shape[0])
-    for k in range(Xtest.shape[0]):
-        tmp = Xtest[k] - mu_k
-        lbls[k] = np.argmin(np.apply_along_axis(np.linalg.norm, 1, tmp))
-
-    return lbls
-    # lbls
-    # tmp = test_images[s_test[0]] - mu_k
-    # k = np.apply_along_axis(np.linalg.norm, 1, tmp)
-    # lbl = np.argmin(k)
-    # print("not yet implemented")
-
 
 #ORL training data and label import
 ORL_images = np.transpose(np.genfromtxt('./data/orl_data.txt', delimiter=""))
@@ -47,9 +20,6 @@ training_labels = np.asarray(training_labels)
 test_images = np.asarray(test_images)
 test_labels = np.asarray(test_labels)
 
-#x = np.array([[3,2],[4,5]])
-#y = np.array([2,1])
-
 
 #Shuffle dataset by creating indexes
 s_training = np.arange(training_images.shape[0])
@@ -57,19 +27,6 @@ np.random.shuffle(s_training)
 
 s_test = np.arange(test_images.shape[0])
 np.random.shuffle(s_test)
-
-#train_shuffle = np.asarray(training_images)
-#h = [training_images, training_labels]
-#shuf = np.arange(X.shape[0])
-#h = np.array()
-
-
-#Shuffle data
-
-#h = np.apply_along_axis(np.linalg.norm, 1, x)
-
-#train_shuffle = h[0]
-#lbls_shuffle = h[1]
 
 #Show MNIST image i
 #plt.imshow(np.reshape(training_images[s_training[0]],(28,28)),cmap='gray')
@@ -83,25 +40,30 @@ np.random.shuffle(s_test)
 # plt.show()
 
 
+lbls = alg.nc_classify(training_images,test_images,training_labels,10)
+
+print(alg.error_in_percent(test_labels,lbls))
 
 
-lbl = nc_classify(training_images,test_images,training_labels,10)
+#Shuffle dataset by creating indexes
+s_ORL = np.arange(ORL_images.shape[0])
+np.random.shuffle(s_ORL)
 
-# count = 0
-#
-# mu_k = np.zeros((10,784))
-# N_k = np.zeros(10)
-#
-# for i in range(s_training.shape[0]):
-#     mu_k[training_labels[i]] += training_images[i]
-#     N_k[training_labels[i]] += 1
-#
-# for k in range(N_k.shape[0]):
-#     mu_k[k] = np.true_divide(mu_k[k], N_k[k])
-#
-# tmp = test_images[s_test[0]] - mu_k
-# k = np.apply_along_axis(np.linalg.norm, 1,tmp)
-# lbl = np.argmin(k)
-#
-# print('Guess : ',lbl)
-# print('Actual : ', test_labels[s_test[0]])
+#Center labels to 0
+ORL_lbls -= 1
+
+
+#SPLIT ORL in training/ test
+
+tmp1 = np.split(ORL_images[s_ORL],[350])
+tmp2 = np.split(ORL_lbls[s_ORL],[350])
+
+ORL_training_images = tmp1[0]
+ORL_test_images = tmp1[1]
+
+ORL_training_lbls = tmp2[0]
+ORL_test_lbls = tmp2[1]
+
+ORL_heplbls = alg.nc_classify(ORL_training_images,ORL_test_images,ORL_training_lbls,40)
+
+print(alg.error_in_percent(ORL_test_lbls,ORL_heplbls))
