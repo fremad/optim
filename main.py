38288baabaa59dd201c8f17592bf56bf4a19c59a
larsh <larshjerrild@hotmail.com>
2018-11-22 20:59:56 +0100

    Perceptron function added

--- main.py
+++ main.py
@@ -47,3 +47,53 @@ print("MNIST MSE classifier error: ", alg.error_in_percent(MNIST_MSE_lbls,MNIST_
 # nn_test_labels = alg.nn_classify(MNIST_training_labels,MNIST_test_images,MNIST_training_labels)
 
 
+
+
+def perceptron(learning_rate,class_one,class_two,initial_mean):
+
+    w = initial_mean
+
+    # Input altered with [1 x^t]^t
+    aug_c1 = np.ones((class_one.shape[0], class_one.shape[1] + 1))
+    aug_c1[:, :-1] = class_one
+
+    aug_c2 = np.ones((class_two.shape[0], class_two.shape[1] + 1))
+    aug_c2[:, :-1] = C2
+
+    while(True):
+
+        #print(w)
+        c1_guess = w.dot(np.transpose(aug_c1))
+        c2_guess = w.dot(np.transpose(aug_c2))
+
+        c1_misslabel = np.argwhere(c1_guess[0] < 0)
+        c2_misslabel = np.argwhere(c2_guess[0] >= 0)
+
+
+
+        if(c1_misslabel.any() == False):
+            if(c2_misslabel.any() == False):
+                #print("hep")
+                #print(c1_guess,c2_guess,c2_misslabel)
+                break
+
+
+        gradient = np.sum(aug_c1[c1_misslabel],axis=0)-np.sum(aug_c2[c2_misslabel],axis=0)
+        #print(gradient)
+
+        w = w + learning_rate * gradient
+
+    return w
+
+#learning_rate = 0.01
+
+C1 = np.array([[-1,0],[0,-1],[-0.5,-0.5],[-1.5,-1.5],[-2,0],[0,-2],[-1,-1.3]])
+
+C2 = np.array([[1,1],[1.3,0.7],[0.7,1.3],[2.5,1],[0,1]])
+
+
+w = np.array([[0.1, 0.1, 0]])
+
+w = perceptron(0.01,C1,C2,w)
+
+
